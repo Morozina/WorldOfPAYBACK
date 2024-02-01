c544bd27efc9822e6158ec935acb25c277ef70eb
Morozina <zaryadina2014@gmail.com>
2024-02-01 22:11:36 +0100

    Create proj

---
+++ WorldOfPAYBACK/WorldOfPAYBACKApp.swift
@@ -0,0 +1,17 @@
+//
+//  WorldOfPAYBACKApp.swift
+//  WorldOfPAYBACK
+//
+//  Created by Vladyslav Moroz on 01/02/2024.
+//
+
+import SwiftUI
+
+@main
+struct WorldOfPAYBACKApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
